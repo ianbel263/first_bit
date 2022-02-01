8eae4552a096801a5c4c90dfb7d06264495e07f1
Andrey Belyaev <ianbel263@gmail.com>
2022-02-01 15:59:40 +0300

    fix bug

--- public/backend.php
+++ public/backend.php
@@ -2,10 +2,6 @@
 require_once('init.php');
 require_once('main/functions.php');
 
-if (get_user()) {
-    die();
-}
-
 $response_data = [
     'result' => '',
     'data' => [],
@@ -14,41 +10,47 @@ $response_data = [
 
 if ($link) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $json = file_get_contents('php://input');
-        $post_data = json_decode($json, true);
-        $errors = validate($post_data);
-
-        if (count($errors)) {
+        if (get_user()) {
             $response_data['result'] = 'failure';
             $response_data['data'] = [];
-            $response_data['errors'] = $errors;
+            $response_data['errors'] = ['user' => 'Пользователь уже авторизован'];
         } else {
-            $username = mysqli_real_escape_string($link, $post_data['username']);
-            $sql_query = 'SELECT * FROM user WHERE username = ?';
-            $stmt = mysqli_prepare($link, $sql_query);
-            mysqli_stmt_bind_param($stmt, 's', $username);
-            mysqli_stmt_execute($stmt);
-            $res = mysqli_stmt_get_result($stmt);
-            $user = $res ? mysqli_fetch_array($res, MYSQLI_ASSOC) : null;
+            $json = file_get_contents('php://input');
+            $post_data = json_decode($json, true);
+            $errors = validate($post_data);
 
-            if ($user) {
-                if ($user['pwd'] == $post_data['password']) {
-                    $_SESSION['user'] = $user;
-                } else {
-                    $errors['password'] = 'Неверный пароль';
-                }
-            } else {
-                $errors['username'] = 'Такой пользователь не найден';
-            }
-
-            if ($user && empty($errors)) {
-                $response_data['result'] = 'success';
-                $response_data['data'] = $user;
-                $response_data['errors'] = [];
-            } else {
+            if (count($errors)) {
                 $response_data['result'] = 'failure';
                 $response_data['data'] = [];
                 $response_data['errors'] = $errors;
+            } else {
+                $username = mysqli_real_escape_string($link, $post_data['username']);
+                $sql_query = 'SELECT * FROM user WHERE username = ?';
+                $stmt = mysqli_prepare($link, $sql_query);
+                mysqli_stmt_bind_param($stmt, 's', $username);
+                mysqli_stmt_execute($stmt);
+                $res = mysqli_stmt_get_result($stmt);
+                $user = $res ? mysqli_fetch_array($res, MYSQLI_ASSOC) : null;
+
+                if ($user) {
+                    if ($user['pwd'] == $post_data['password']) {
+                        $_SESSION['user'] = $user;
+                    } else {
+                        $errors['password'] = 'Неверный пароль';
+                    }
+                } else {
+                    $errors['username'] = 'Такой пользователь не найден';
+                }
+
+                if ($user && empty($errors)) {
+                    $response_data['result'] = 'success';
+                    $response_data['data'] = $user;
+                    $response_data['errors'] = [];
+                } else {
+                    $response_data['result'] = 'failure';
+                    $response_data['data'] = [];
+                    $response_data['errors'] = $errors;
+                }
             }
         }
 
